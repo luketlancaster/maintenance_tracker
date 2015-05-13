45cf4de3655b3a62f47774f335273997d2f8aa7c
Luke Lancaster <luketlancaster@gmail.com>
2015-05-13 12:59:03 -0500

    add bootstrapping to rakefile and setup to helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,26 @@
 require 'rubygems'
 require 'bundler/setup'
 require "minitest/reporters"
+Dir["./app/**/*.rb"].each { |f| require f }
+Dir["./lib/*.rb"].each { |f| require f }
+ENV["TEST"] = 'true'
 
 reporter_options = { color: true }
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
 
 require 'minitest/autorun'
+class Minitest::Test
+  def setup
+    Database.load_structure
+    Database.execute("DELETE FROM cars;")
+    Database.execute("DELETE FROM tasks;")
+  end
+end
+
+def create_car(year, make, model)
+  Database.execute("INSERT INTO cars (year, make, model) VALUES (?,?,?)", year, make, model)
+end
+
+def create_task(car_id, name, mileage)
+  Database.execute("INSERT INTO tasks (car_id, name, mileage) VALUES (?,?,?)", car_id, name, mileage)
+end
